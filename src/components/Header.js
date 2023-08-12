b7aefbfcea4019e5256713aac2e303eaf8c73077
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-08-12 23:14:08 +0500

    Find the right image, and add it correctly

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react"
+import trollFaceImage from "../images/troll-face.png"; 
 
 export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./images/troll-face.webp" 
+                src={trollFaceImage}
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
